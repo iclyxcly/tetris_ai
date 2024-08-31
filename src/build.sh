dbe94c12eafcb7b0db16d07f923c7a08d7d73885
ICLY <vertyopaster@GMAIL.COM>
2024-08-31 15:52:42 +0800

    Update build.sh

--- src/build.sh
+++ src/build.sh
@@ -6,4 +6,4 @@ if [ ! -f $1.cpp ]; then
     echo "File not found!"
     exit 1
 fi
-clang++ -o $1 $1.cpp -Ofast -march=native -std=c++23 -lixwebsocket && ./$1
+clang++ -o $1 $1.cpp -Ofast -march=native -std=c++20 -lixwebsocket && ./$1
