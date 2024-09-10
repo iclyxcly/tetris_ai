791a23f5a67b86b6d867a55efbc5f86256697903
ICLY <vertyopaster@GMAIL.COM>
2024-09-10 16:44:34 +0800

    test on hashing algorithm

--- src_lab/movegen.hpp
+++ src_lab/movegen.hpp
@@ -1,5 +1,6 @@
 #pragma once
 #include "board.hpp"
+#include "mino.hpp"
 #include <queue>
 #include <vector>
 namespace moenew
@@ -9,9 +10,18 @@ namespace moenew
 	public:
 		Board &target;
 		Minos::Active &cur;
-		Minos::Active &cur;
 		std::queue<Minos::Active> search;
-		std::vector<bool> visited, result;
-		MoveGen(Board &target, Minos::Active &init);
+		std::vector<bool> visited;
+		std::vector<bool> result;
+		uint16_t hashify(const Minos::Active &mino) const
+		{
+			return mino.x + mino.y * target.w + mino.r * 4;
+		}
+		MoveGen(Board &target, Minos::Active &init) : target(target), cur(init)
+		{
+			visited.resize(4352);
+			result.resize(1024);
+			search.push(init);
+		}
 	};
 }
